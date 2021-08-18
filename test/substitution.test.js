f87e0a8bf684fe9b4fb3d71fc7476bb137f40b31
Tyler Williams <tylerwilliams1737@gmail.com>
2021-08-18 08:39:39 -0700

    added tests

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,35 @@
-// Write your tests here!
+const expect = require('chai').expect;
+const { substitution } = require('../src/substitution');
+
+describe('substitution', () => {
+  it("Should encode a message by using the given substitution alphabet", () => {
+    let message = "thinkful";
+    let actual = substitution(message, "xoyqmcgrukswaflnthdjpzibev");
+    expect(actual).to.equal("jrufscpw");
+  });
+  it("Should encode a message by using the given substitution alphabet", () => {
+    let message = "thinkful";
+    let actual = substitution(message, "xoyqmcgrukswaflnthdjpzibev");
+    expect(actual).to.equal("jrufscpw");
+  });
+  it("Should encode a message by using the given substitution alphabet", () => {
+    let message = "thinkful";
+    let actual = substitution(message, "xoyqmcgrukswaflnthdjpzibev");
+    expect(actual).to.equal("jrufscpw");
+  });
+  it("Should encode a message by using the given substitution alphabet", () => {
+    let message = "thinkful";
+    let actual = substitution(message, "xoyqmcgrukswaflnthdjpzibev");
+    expect(actual).to.equal("jrufscpw");
+  });
+  it("Should encode a message by using the given substitution alphabet", () => {
+    let message = "thinkful";
+    let actual = substitution(message, "xoyqmcgrukswaflnthdjpzibev");
+    expect(actual).to.equal("jrufscpw");
+  });
+  it("Should encode a message by using the given substitution alphabet", () => {
+    let message = "thinkful";
+    let actual = substitution(message, "xoyqmcgrukswaflnthdjpzibev");
+    expect(actual).to.equal("jrufscpw");
+  });
+})
